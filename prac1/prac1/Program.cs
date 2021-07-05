ea8891cd0f2d51d598c4b939dabd2351376dcf3b
Андрей Cидоров <aqaatron@gmail.com>
2021-07-05 17:02:51 +0400

    prac 1 completed

--- prac1/prac1/Program.cs
+++ prac1/prac1/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.Json;
 
 namespace prac1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+
+            StreamWriter SW = new StreamWriter(new FileStream("Result.txt", FileMode.Create, FileAccess.Write));
+
+            for (int i = 4; i < 14; i++)
+            {
+                HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts/" + i.ToString());
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    HttpContent responseContent = response.Content;
+
+                    var json = await responseContent.ReadAsStreamAsync();
+
+                    var post = await JsonSerializer.DeserializeAsync<Post>(json);
+
+
+                    SW.WriteLine(post.userId.ToString());
+
+                    SW.WriteLine(post.id.ToString());
+
+                    SW.WriteLine(post.title);
+
+                    SW.WriteLine(post.body);
+
+                    SW.WriteLine();
+
+
+
+
+                }
+            }
+            SW.Close();
+
+
         }
     }
+
+    public class Post
+    {
+        public int userId { get; set; }
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public string body { get; set; }
+    }
+
 }
